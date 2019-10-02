0df98b94897d743266f851e352c8d81e9fe49e25
Abdulla Mian <miana@claycorp.com>
2019-10-02 16:31:01 -0500

    encryption complete

---
+++ RotNKata/RotN.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace RotNKata
+{
+    public class RotN
+    {
+
+        public string Encrypt(int rot, string text)
+        {
+            char[] splitString = text.ToCharArray();
+            string encryptedText = "";
+
+            foreach (char letter in splitString) 
+            {
+                encryptedText = string.Concat(encryptedText, EncryptSingleLetter(rot, letter.ToString()));
+            }
+            return encryptedText;
+        }
+
+        public string Decrypt(int rot, string text)
+        {
+            return Encrypt(-rot, text);
+        }
+
+        public string EncryptSingleLetter(int rot, string text)
+        {
+            if(String.IsNullOrEmpty(text)) return "";
+            List<string> alphabet = Alphabet();
+            int indexOfLetter = alphabet.IndexOf(text);
+            return alphabet[IndexOfCipherLetter(indexOfLetter, rot)];
+        }
+
+        private int IndexOfCipherLetter(int indexOfPlaintextLetter, int rot)
+        {
+            if(indexOfPlaintextLetter - rot < 0)
+            {
+                return indexOfPlaintextLetter - rot + Alphabet().Count;
+            }
+            else
+            {
+                return indexOfPlaintextLetter - rot;
+            }
+        }
+
+        private static List<string> Alphabet()
+        {
+            List<string> letters = new List<string>();
+            for (char c = 'A'; c <= 'Z'; c++) 
+            {
+                letters.Add(c.ToString());
+            }
+            return letters;
+        }
+    }
+}
---
+++ RotNTests/RotNTests.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using RotNKata;
+using System;
+
+namespace RotNTests
+{
+    public class RotNTests
+    {
+        private RotN rn;
+
+        [SetUp]
+        public void SetUp()
+        {
+            rn = new RotN();
+
+        }
+
+        [Test]
+        public void CreateCipher()
+        {
+            Assert.AreEqual("", rn.Encrypt(0, ""));
+        }
+
+        [Test]
+        public void RotateByOne()
+        {
+            Assert.AreEqual("A", rn.Encrypt(1, "B"));
+        }
+
+
+        [Test]
+        public void RotateByTwo()
+        {
+            Assert.AreEqual("A", rn.Encrypt(2, "C"));
+        }
+
+        [Test]
+        public void RotateLetterCByOne()
+        {
+            Assert.AreEqual("B", rn.Encrypt(1, "C"));
+        }
+
+        [Test]
+        public void RotateLetterDByThree()
+        {
+            Assert.AreEqual("A", rn.Encrypt(3, "D"));
+        }
+
+        [Test]
+        public void RotateLetterZBy12()
+        {
+            Assert.AreEqual("N", rn.Encrypt(12, "Z"));
+        }
+
+        [Test]
+        public void RotateLetterABy1()
+        {
+            Assert.AreEqual("Z", rn.Encrypt(1, "A"));
+        }
+
+        [Test]
+        public void RotateLetterFBy7()
+        {
+            Assert.AreEqual("Y", rn.Encrypt(7, "F"));
+        }
+
+        [Test]
+        public void RotateLetterFBy9()
+        {
+            Assert.AreEqual("W", rn.Encrypt(9, "F"));
+        }
+
+        [Test]
+        public void RotateMultipleLettersBy5()
+        {
+            Assert.AreEqual("XB", rn.Encrypt(5, "CG"));
+        }
+
+        [Test]
+        public void EncryptAndDecrypt()
+        {
+            string ciphertext = rn.Encrypt(10, "ABCDEFG");
+            string plaintext = rn.Decrypt(10, ciphertext);
+            Assert.AreEqual(plaintext, "ABCDEFG");
+        }
+    }
+}
