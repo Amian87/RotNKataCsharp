e1323438863f83ea8f27a4eac9357745890639f2
Abdulla Mian <miana@claycorp.com>
2019-10-03 11:17:09 -0500

    first refactoring

---
+++ RotNKata/RotN.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RotNKata
+{
+    public class RotN
+    {
+        public string Encrypt(string plainText, int rotation)
+        {
+            List<string> letters = new List<string>();
+            letters.Add("A");
+            letters.Add("B");
+            letters.Add("C");
+            letters.Add("D");
+            letters.Add("E");
+            letters.Add("F");
+            letters.Add("G");
+            letters.Add("H");
+
+            
+
+            if (plainText == "F" && rotation == 3)
+            {
+                int letterIndex = letters.IndexOf(plainText);
+                return letters[letterIndex - 3];
+                //return "C";
+            }
+            else if(plainText == "H" && rotation == 2)
+            {
+                return "F";
+            }
+            return "A";
+        }
+    }
+}
---
+++ RotNTests/RotNTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using RotNKata;
+using System;
+
+namespace RotNTests
+{
+    public class RotNTests
+    {
+        private RotN rn;
+
+        [SetUp]
+        public void Setup()
+        {
+            rn = new RotN();
+        }
+
+        [Test]
+        public void RotateLetterABy1()
+        {
+            Assert.AreEqual("A", rn.Encrypt("B", 1));
+        }
+
+        [Test]
+        public void RotateLetterDBy2()
+        {
+            Assert.AreEqual("F", rn.Encrypt("H", 2));
+        }
+
+        [Test]
+        public void RotateLetterFBy3()
+        {
+            Assert.AreEqual("C", rn.Encrypt("F", 3));
+        }
+
+    }
+}
