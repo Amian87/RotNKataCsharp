4c50e6fea36d5b53b3ae89898892e28aa732985a
Abdulla Mian <miana@claycorp.com>
2019-10-07 17:27:00 -0500

    incomplete

---
+++ RotNKata/RotN.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RotNKata
+{
+    public class RotN
+    {
+        public string Encrypt(int rotation, string plainText)
+        {
+            char[] charPlainText = plainText.ToCharArray();
+
+            string encryptedText = "";
+
+            foreach (char c in charPlainText)
+            {
+                encryptedText = string.Concat(encryptedText, encryptSingleLetter(rotation, c.ToString()));
+            }
+            return encryptedText;
+        }
+
+        private static string encryptSingleLetter(int rotation, string plainText)
+        {
+            List<string> alphabets = listOfAlphabets();
+            int alphabetIndex = alphabets.IndexOf(plainText);
+
+            if (alphabetIndex - 1 < 0)
+            {
+                return alphabets[alphabetIndex - rotation + alphabets.Count];
+            }
+            else
+            {
+
+                return alphabets[alphabetIndex - rotation];
+            }
+        }
+
+        private static List<string> listOfAlphabets()
+        {
+            List<string> alphabets = new List<string>();
+            
+            for(char c = 'A'; c <= 'Z'; c++)
+            {
+                alphabets.Add(c.ToString());
+            }
+            return alphabets;
+        }
+    }
+}
---
+++ RotNTests/RotNTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using RotNKata;
+using System;
+
+namespace RotNTests
+{
+    public class RotNTests
+    {
+        private RotN rn;
+
+        [SetUp]
+        public void SetUp()
+        {
+            rn = new RotN();
+        }
+
+        [Test]
+        public void RotateLetterCBy1()
+        {
+            Assert.AreEqual("B", rn.Encrypt(1, "C"));
+        }
+
+        [Test]
+        public void RotateLetterDBy3()
+        {
+            Assert.AreEqual("A", rn.Encrypt(3, "D"));
+        }
+
+        [Test]
+        public void RotateLetterEBy1()
+        {
+            Assert.AreEqual("D", rn.Encrypt(1, "E"));
+        }
+
+        [Test]
+        public void RotateLetterGBy2()
+        {
+            Assert.AreEqual("E", rn.Encrypt(2, "G"));
+        }
+
+        [Test]
+        public void RotateLetterABy1()
+        {
+            Assert.AreEqual("Z", rn.Encrypt(1, "A"));
+        }
+
+        [Test]
+        public void RotateMulitpleLettersXBBy2()
+        {
+            Assert.AreEqual("VZ", rn.Encrypt(2, "XB"));
+        }
+
+
+    }
+}
