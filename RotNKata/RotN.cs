f018bc08c403f171110501a4a4d2aaa3fec8cbcd
Abdulla Mian <miana@claycorp.com>
2019-10-09 11:10:40 -0500

    encrypt and decrypt complete

---
+++ RotNKata/RotN.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RotNKata
+{
+    public class RotN
+    {
+        public string Decrypt(int rotation, string cipherText)
+        {
+            string plainText = "";
+
+            foreach (char c in cipherText)
+            {
+               plainText = string.Concat(plainText, DecryptSingleLetter(rotation, c.ToString())); 
+            }
+
+            return plainText;
+
+
+
+        }
+
+        private static string DecryptSingleLetter(int rotation, string cipherText)
+        {
+            List<string> alphabets = listOfAlphabets();
+            int alphabetIndex = alphabets.IndexOf(cipherText);
+
+            if (alphabetIndex + rotation > alphabets.Count - 1)
+            {
+                return alphabets[alphabetIndex + rotation - alphabets.Count];
+            }
+
+            return alphabets[alphabetIndex + rotation];
+        }
+
+        public string Encrypt(int rotation, string plainText)
+        {
+            string encryptedText = "";
+
+            foreach (char c in plainText)
+            {
+                encryptedText = string.Concat(encryptedText, encryptSingleLetter(rotation, c.ToString())); 
+            }
+
+            return encryptedText;
+
+        }
+
+        private static string encryptSingleLetter(int rotation, string plainText)
+        {
+            List<string> alphabets = listOfAlphabets();
+            int alpahbetIndex = alphabets.IndexOf(plainText);
+
+
+            if (alpahbetIndex - rotation < 0)
+            {
+                return alphabets[alpahbetIndex - rotation + alphabets.Count];
+            }
+
+            return alphabets[alpahbetIndex - rotation];
+        }
+
+        private static List<string> listOfAlphabets()
+        {
+            List<string> alphabets = new List<string>();
+
+            for(char c = 'A'; c <= 'Z'; c++)
+            {
+                alphabets.Add(c.ToString());
+            }
+            return alphabets;
+        }
+    }
+}
---
+++ RotNTests/RotNTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using RotNKata;
+using System;
+
+namespace RotNTests
+{
+    public class RotNTests
+    {
+        private RotN rn;
+
+        [SetUp]
+        public void SetUp()
+        {
+           rn = new RotN();
+        }
+
+        [Test]
+        public void RotateLetterBBy1()
+        {
+            Assert.AreEqual("A", rn.Encrypt(1, "B"));
+        }
+
+        [Test]
+        public void RotateLetterDBy2()
+        {
+            Assert.AreEqual("B", rn.Encrypt(2, "D"));
+        }
+
+        [Test]
+        public void RotateLetterFBy2()
+        {
+            Assert.AreEqual("D", rn.Encrypt(2, "F"));
+        }
+
+        [Test]
+        public void RotateLetterGBy2()
+        {
+            Assert.AreEqual("E", rn.Encrypt(2, "G"));
+        }
+
+        [Test]
+        public void RotateLetterABy1()
+        {
+            Assert.AreEqual("Z", rn.Encrypt(1, "A"));
+        }
+
+        [Test]
+        public void RotateMultipleLettersXBBy2()
+        {
+            Assert.AreEqual("VZ", rn.Encrypt(2, "XB"));
+        }
+
+        [Test]
+        public void DecyptyLetterAWithRotation1()
+        {
+            Assert.AreEqual("B", rn.Decrypt(1, "A"));
+        }
+
+        [Test]
+        public void DecyptyLetterCWithRotation2()
+        {
+            Assert.AreEqual("E", rn.Decrypt(2, "C"));
+        }
+
+        [Test]
+        public void DecyptyLetterFWithRotation1()
+        {
+            Assert.AreEqual("G", rn.Decrypt(1, "F"));
+        }
+
+        [Test]
+        public void DecyptyLetterGWithRotation2()
+        {
+            Assert.AreEqual("I", rn.Decrypt(2, "G"));
+        }
+
+        [Test]
+        public void DecyptyLetterZWithRotation2()
+        {
+            Assert.AreEqual("B", rn.Decrypt(2, "Z"));
+        }
+
+        [Test]
+        public void DecyptyMultipleLettersVZWithRotation2()
+        {
+            Assert.AreEqual("XB", rn.Decrypt(2, "VZ"));
+        }
+    }
+}
