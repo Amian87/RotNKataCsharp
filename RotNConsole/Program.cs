efdece6d89db6b6f5ac80450fa0844a8914e5220
Abdulla Mian <miana@claycorp.com>
2020-07-06 00:11:06 -0500

    complete

---
+++ RotNConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RotNKata;
+
+namespace RotNConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RotN rotN = new RotN();
+            Console.WriteLine(rotN.Encrypt("CG", 5));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RotNKata.Test/RotNTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RotNKata.Test
+{
+    public class RotNTest
+    {
+        private RotN rotN;
+
+        [SetUp]
+        public void SetUpRotN()
+        {
+            rotN = new RotN();
+        }
+
+        [Test]
+        public void RotateLetterAByOne()
+        {
+            Assert.AreEqual("A", rotN.Encrypt("B", 1));
+        }
+
+        [Test]
+        public void RotateLetterByTwo()
+        {
+            Assert.AreEqual("F", rotN.Encrypt("H", 2) );
+        }
+
+        [Test]
+        public void RotateLetterByThree()
+        {
+            Assert.AreEqual("C", rotN.Encrypt("F", 3));
+        }
+
+        [Test]
+        public void RotateLetterIByFour()
+        {
+            Assert.AreEqual("E", rotN.Encrypt("I", 4));
+        }
+
+        [Test]
+        public void RotateMultipleLettersByFive()
+        {
+            Assert.AreEqual("XB", rotN.Encrypt("CG", 5));
+        }
+
+    }
+}
