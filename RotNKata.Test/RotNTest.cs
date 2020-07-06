b2b1674dc517d29f9c3aa89889ea047f278516c9
Abdulla Mian <miana@claycorp.com>
2020-07-06 04:33:47 -0500

    complete

---
+++ RotNKata.Test/RotNTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Security;
+using NUnit.Framework;
+
+namespace RotNKata.Test
+{
+    public class RotNTest
+    {
+        private RotN rotN;
+
+        [SetUp]
+        public void SetUpRotN()
+        {
+            rotN = new RotN();
+        }
+
+        [Test]
+        public void RotateLetterBByOne()
+        {
+            Assert.AreEqual("A", rotN.Encrypt("B", 1));
+        }
+
+        [Test]
+        public void RotateLetterDByTwo()
+        {
+            Assert.AreEqual("B", rotN.Encrypt("D", 2));
+        }
+
+        [Test]
+        public void RotateLetterDByThree()
+        {
+            Assert.AreEqual("A", rotN.Encrypt("D", 3));
+        }
+
+        [Test]
+        public void RotateMultipleLettersBy5()
+        {
+            Assert.AreEqual("XB", rotN.Encrypt("CG", 5));
+        }
+
+    }
+}
---
+++ RotNKata/RotN.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+public class RotN
+{
+    public string Encrypt(string plainText, int rotation)
+    {
+        char[] plainTextCharArray = plainText.ToCharArray();
+        string encryptedText = "";
+
+        foreach (var letter in plainTextCharArray)
+        {
+            encryptedText = string.Concat(encryptedText, EncryptSingleLetter(rotation, letter.ToString()));
+        }
+
+        return encryptedText;
+    }
+
+    private static string EncryptSingleLetter(int rotation, string plainText)
+    {
+        List<string> alphabets = Alphabets();
+        int plainTextIndex = alphabets.IndexOf(plainText);
+
+        if (plainTextIndex - rotation < 0)
+        {
+            return alphabets[plainTextIndex - rotation + alphabets.Count];
+        }
+        else
+        {
+            return alphabets[plainTextIndex - rotation];
+        }
+    }
+
+    private static List<string> Alphabets()
+    {
+        List<string> alphabets = new List<string>();
+
+        for (char c = 'A'; c <= 'Z'; c++)
+        {
+            alphabets.Add(c.ToString());
+        }
+
+        return alphabets;
+    }
+}
